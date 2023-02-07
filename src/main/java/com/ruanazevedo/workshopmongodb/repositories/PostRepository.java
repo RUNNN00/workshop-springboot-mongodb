dd6f040a3046116cd8425ef4d918f332ba17c0b5
ruanazevedo <ruannzeroh@gmail.com>
2023-02-07 15:34:51 -0300

    consulta simples com query methods

--- src/main/java/com/ruanazevedo/workshopmongodb/repositories/PostRepository.java
+++ src/main/java/com/ruanazevedo/workshopmongodb/repositories/PostRepository.java
@@ -1,7 +1,12 @@
 package com.ruanazevedo.workshopmongodb.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.ruanazevedo.workshopmongodb.domain.Post;
 
-public interface PostRepository extends MongoRepository<Post, String> {}
+public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
+}
